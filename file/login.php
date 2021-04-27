16fb2d1ccf8a7e787853a2dbaf8f8ba647e9a32e
ardianadm <ardian.adam16@gmail.com>
2021-04-27 14:38:16 +0700

    ngedit login page lokasi stylesheet di folder file

--- file/login.php
+++ file/login.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link href="https://fonts.googleapis.com/css2?family=Montserrat&display=swap" rel="stylesheet"/>
-    <link rel="stylesheet" href="css/login.css" />
+    <link rel="stylesheet" href="../css/login.css" />
     <title> Log In </title>
   </head>
   <body>
@@ -13,7 +13,7 @@
       <div class="outer">
         <div class="login">
           <!-- <h1> Dewata <br/> Trans</h1> -->
-          <img src="img/Logo Dewata.png" alt="">
+          <img src="../img/Logo Dewata.png" alt="">
     
           <form action="" method="post">
             <input type="text" name="username" id="username" placeholder="Username" style="margin-bottom: 15px"/>
@@ -33,7 +33,7 @@
         <h1> Dewata Trans Malang </h1>
         <p> Kemudahan dan kenyamanan dalam mendapatkan informasi mengenai rental mobil Dewata Trans Malang. </p>
         <!-- <a href=""> SIGN UP </a> <br> -->
-        <img src="img/mobil_cover.png" alt="">
+        <img src="../img/mobil_cover.png" alt="">
       </div>
     </section>
   </body>
