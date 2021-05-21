639a184bd1674f7c7fe3c3467f589ebab8d69fab
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-05-21 20:33:44 +0700

    update 1

---
+++ file/index.php
@@ -0,0 +1,20 @@
+<?php include "./_partials/head.php" ?>
+
+<link rel="stylesheet" href="../css/index.min.css">
+
+<body>
+    <header>
+
+    </header>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="sidebar col-2 ">
+                <img src="../img/Group80.png" alt="">
+            </div>
+            <div class="workspace col-auto"></div>
+        </div>
+    </div>
+
+</body>
+
+</html>
