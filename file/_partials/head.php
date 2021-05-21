fbfc77cf1c2a563378ecf7d3ba24b134e240ca21
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-05-21 20:42:26 +0700

    import fa

--- file/_partials/head.php
+++ file/_partials/head.php
@@ -7,5 +7,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../css/responsive.min.css">
     <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous"> -->
+    <link href="../../asset/font-awesome/css/all.min.css" rel="stylesheet">
+    <script defer src="../../asset/font-awesome/js/all.js"></script>
+    <!--load all styles -->
     <title>Dewata Trans</title>
 </head>
