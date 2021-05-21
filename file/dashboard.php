bdcd6f5940d957b21cfecebf50d34c2d00d4da3d
ErlanggaDewa <erlanggadewa7@gmail.com>
2021-05-22 06:29:46 +0700

    update

---
+++ file/dashboard.php
@@ -0,0 +1,32 @@
+<link rel="stylesheet" href="../css/dashboard.min.css">
+<h1>Dashboard Aplikasi Rental Mobil</h1>
+<hr>
+<div class="row wrapper-data">
+    <div class="data1 mobil col-12 col-md-6 col-xl-4">
+        <div class="data-mobil">
+            <div class="info-data">
+                <h3>Data Mobil</h3>
+                <span>2 Mobil</span>
+            </div>
+            <i class="fas fa-car"></i>
+        </div>
+    </div>
+    <div class="data2 pemesan col-12 col-md-6 col-xl-4">
+        <div class="data-pemesan">
+            <div class="info-data">
+                <h3>Data Pemesan</h3>
+                <span>3 Pemesan</span>
+            </div>
+            <i class="fas fa-user-clock"></i>
+        </div>
+    </div>
+    <div class="data3 pesanan col-12 col-md-6 col-xl-4">
+        <div class="data-pesanan">
+            <div class="info-data">
+                <h3>Data Mobil</h3>
+                <span>1 Pesanan</span>
+            </div>
+            <i class="fas fa-server"></i>
+        </div>
+    </div>
+</div>
--- file/index.php
+++ file/index.php
@@ -3,15 +3,59 @@
 <link rel="stylesheet" href="../css/index.min.css">
 
 <body>
-    <header>
+    <!-- <header>
 
-    </header>
+    </header> -->
     <div class="container-fluid">
         <div class="row">
-            <div class="sidebar col-2 ">
+            <div class="sidebar col-2">
                 <img src="../img/Group80.png" alt="">
+                <div class="navbar">
+                    <i class="fas fa-house-user">
+                        <span>Dashboard</span>
+                    </i>
+                    <div class="grup-nav master-mobil">
+                        <h6>Master Mobil</h6>
+                        <i class="fas fa-car">
+                            <span>Data Kendaraan</span>
+                        </i>
+                    </div>
+                    <div class="grup-nav status">
+                        <h6>Status</h6>
+                        <i class="fas fa-envelope-open-text">
+                            <span>Pending</span>
+                        </i>
+                        <i class="fas fa-route">
+                            <span>On Going</span>
+                        </i>
+                    </div>
+                    <div class="grup-nav rental-mobil">
+                        <h6>Rental Mobil</h6>
+                        <i class="fas fa-folder-open">
+                            <span>Input Pemesan</span>
+                        </i>
+                        <i class="fas fa-database">
+                            <span>Data Pesanan</span>
+                        </i>
+                    </div>
+                    <div class="grup-nav paket-pariwisata">
+                        <h6>Paket Pariwisata</h6>
+                        <i class="fas fa-user-friends">
+                            <span>List Wisata</span>
+                        </i>
+                        <i class="fas fa-folder-open">
+                            <span>Input Pemesan</span>
+                        </i>
+                        <i class="fas fa-database">
+                            <span>Data Pesanan</span>
+                        </i>
+                    </div>
+                </div>
             </div>
-            <div class="workspace col-auto"></div>
+            <div class="workspace col-10">
+                <!-- NANTI INClUDE KERJAAN KALIAN DISINI YA, YANG LAIN DI COMMENT BUAT NGETES -->
+                <?php include "dashboard.php" ?>
+            </div>
         </div>
     </div>
 
